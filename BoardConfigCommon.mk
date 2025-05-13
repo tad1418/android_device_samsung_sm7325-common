270b47a8f732144aa29804b371a6d11c2a95a640
tilt-rgb <lesma_chan@pm.me>
2025-05-13 21:19:14 +0000

    sm7325-common: manifest_yupik => manifest_lahaina
    * Needed for dolby compatibility
    
    Signed-off-by: tilt-rgb <lesma_chan@pm.me>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -170,8 +170,7 @@ SOONG_CONFIG_samsungCameraVars_needs_sec_reserved_field := true
 TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):libinit_samsung_sm7325
 
 # HIDL manifests
-DEVICE_MANIFEST_SKUS := yupik
-DEVICE_MANIFEST_YUPIK_FILES += $(COMMON_PATH)/configs/manifest_yupik.xml
+DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/manifest_lahaina.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/configs/framework_compatibility_matrix.xml \
